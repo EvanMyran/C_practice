d709a61677dddc6200808b3302dfec9da986a101
Evan Myran <evankmyran@gmail.com>
2025-06-18 14:39:51 -0500

    Update 01_04-hello1.c

--- CH01/01_04/01_04-hello1.c
+++ CH01/01_04/01_04-hello1.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-	#initialize char string array
-	#add a \n to print new line
+	//initialize char string array
+	//add a \n to print new line
 	char string[] = "Greetings, human!\n";
 	int index;
 
 	index = 0;
 
-	#while loop cmparing every index of the string array and checking that it != '\0'
-	#It would stop printing if there was a \0
+	//while loop cmparing every index of the string array and checking that it != '\0'
+	//It would stop printing if there was a \0
 	while( string[index] != '\0' )
 	{
 		putchar( string[index] );
