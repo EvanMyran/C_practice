0d203d86dca19c64b21815e6a04d9acdcf87367f
Evan Myran <evankmyran@gmail.com>
2025-06-18 14:49:46 -0500

    Update 01_09-define3.c

--- CH01/01_09/01_09-define3.c
+++ CH01/01_09/01_09-define3.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+// define a function charout(a) as the c function putc(a,stdout)
+//putc is a function (or macro) used to write a single character to a specified output stream.
+//putc takes two arguments: a character (typically an integer representing the character's ASCII value) and a stream pointer. 
+// this will output whatever a is
 #define charout(a) putc(a,stdout)
 
 int main()
