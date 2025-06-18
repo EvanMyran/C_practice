922b93bceff62218b5c470572e00a163e4658b76
Evan Myran <evankmyran@gmail.com>
2025-06-18 13:47:14 -0500

    Update 01_05-sample1.c

--- CH01/01_05/01_05-sample1.c
+++ CH01/01_05/01_05-sample1.c
@@ -5,9 +5,14 @@ int main()
 {
 	int a,b;
 
+	#printf function
 	printf("Type a positive value: ");
+
+	#scanf fucntion scans for user input. scanf("%d", &n) reads an integer from the keyboard and stores it in the integer variable n. 
+	#The %d format specifier indicates that an integer is expected, and &n provides the memory address of n so that scanf can store the input value there.
 	scanf("%d",&b);
-	for(a=0;a<b;a++)
+
+	for(a = 0; a < b; a++)
 	{
 		printf("I must do this %d times\n",b);
 		if(a==9)
