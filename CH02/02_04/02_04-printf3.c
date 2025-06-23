eb7104dd2ff6875c66cdff4f03b061751191bd08
Evan Myran <evankmyran@gmail.com>
2025-06-23 15:52:23 -0500

    printf.c

--- CH02/02_04/02_04-printf3.c
+++ CH02/02_04/02_04-printf3.c
@@ -6,7 +6,9 @@ int main()
 	unsigned x = 100;
 	float pi = 3.14159;
 
-	//%u - represents the 
+	//%u - represents the unsigned variable
+	//conversion specifications come in between the %[options]n
+	//example is .2 means 2 numbers after the decimal place
 	printf("ch = %c, x = %u, pi = %f\n",ch,x,pi);
 	return 0;
 }
