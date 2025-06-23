2c6a1a89c33f12e6e96d54fea0714d5d6bea77d1
Evan Myran <evankmyran@gmail.com>
2025-06-23 10:57:09 -0500

    Update 01_13-solution1.c

--- CH01/01_13/01_13-solution1.c
+++ CH01/01_13/01_13-solution1.c
@@ -11,6 +11,30 @@ int main()
 	/* main program loop */
 
 	/* output results */
+	puts("The first C practice challenge!\n");
+    puts("Im gonna make a bunch of honey-combs! Save the bees.\n");
+    for(int rows = 0; rows < 5; rows++) {
+	int a = 0, b = 0, c = 0; // Reset counters for each row
+	for(int a = 0; a < 10; a++) {
+	    printf(" __ ");
+	}
+	putchar('\n');
+	
+	for(int b = 0; b < 10; b++) {
+	    printf("/ \\ " );
+	}
+	putchar('\n');
+	
+	for(int c = 0; c < 10; c++) {
+	    printf("\\ / ");
+	}
+	putchar('\n');
+	
+	for(int d = 0; d < 10; d++) {
+	    printf(" -- ");
+	}
+	putchar('\n');
+    }
 
 	return 0;
 }
