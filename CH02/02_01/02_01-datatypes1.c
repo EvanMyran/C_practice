16de5ead5e40585421d718e83059ef282642bdd3
Evan Myran <evankmyran@gmail.com>
2025-06-23 13:46:28 -0500

    Update 02_01-datatypes1.c

--- CH02/02_01/02_01-datatypes1.c
+++ CH02/02_01/02_01-datatypes1.c
@@ -2,18 +2,28 @@
 
 int main()
 {
-	char c;
-	int i;
-	float f;
-	double d;
+
+	//C HAS 4 MAIN DATA TYPES, CHAR, INT, FLOAT, DOUBLE
+	char c; //SINGLE CHARACTERS + BYTES
+	int i; //INTS OR WHOLE NUMBER  VALUES
+	float f; //REAL NUMBERS, SINGLE PRECSION -- decimals, fractions, and very large values -- accurate to 8 digits 
+	double d; // REAL NUMBERS, DOUBLE PRECISION -- accurate to 16 digits
 
 	/* input */
 	printf("Type a character: ");
+	//scanf(%c - holders for the data type, &c - addressing for the variable
 	scanf("%c",&c);
+
+	//%d - Standard specifier for reading whole numbers
 	printf("Type an integer: ");
 	scanf("%d",&i);
+
+	//%f - fixed point, Reads a single-precision floating-point number
+	//%e - scientific notation
 	printf("Type a small value: ");
 	scanf("%f",&f);
+
+	//%lg - double (long float), tells the compiler to print or scan a double value
 	printf("Type a huge value: ");
 	scanf("%lg",&d);
 
