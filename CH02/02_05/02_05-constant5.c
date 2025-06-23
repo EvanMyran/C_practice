d242937856e2559e3fb1ed3c7fca117832923126
Evan Myran <evankmyran@gmail.com>
2025-06-23 16:15:01 -0500

    Update 02_05-constant5.c

--- CH02/02_05/02_05-constant5.c
+++ CH02/02_05/02_05-constant5.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 
+//defining a constant max tobe the value of 20
 #define MAX 20
 
 /* generate a line */
+
+//void function, has no return value
+//has input int v
 void line(int v)
 {
 	int x;
-
+	//for loop as long as x is less than v
 	for( x=0; x<v; x++ )
 	{
+		//if x is greater than or equal to 20 then break
+		//otherwise pritn -
 		if( x >= MAX )
 			break;
 		putchar('-');
 	}
+	//end for loop with new line
 	putchar('\n');
 }
-
+//main
 int main()
 {
 	int value;
 
+	//user input for a positive value less than 20
 	printf("Enter a positive value less than %d: ",MAX);
+	//scan user value and address it
 	scanf("%d",&value);
+	//call line function and pass the value
 	line(value);
 	return(0);
 }
