b9df0a229cf29498118bcd5526ca3aedb378dfe7
Evan Myran <evankmyran@gmail.com>
2025-06-23 09:42:48 -0500

    Update 01_10-math1.c

--- CH01/01_10/01_10-math1.c
+++ CH01/01_10/01_10-math1.c
@@ -8,9 +8,15 @@
 int main()
 {
 	int w;
+
+	//for loop int w iterate up until 10
 	for( w=0; w<10; w++)
 	{
+		//printing out float with 2 numbers post decimals
+		//the number is the sin of pi over w + 1
 		printf("%.2f\n",sin(M_PI/(w+1)) );
 	}
+
+	//return 0
 	return 0;
 }
