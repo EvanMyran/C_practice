8b63b6e213a70c89d4a57e8c617f7fa6164d17a7
Evan Myran <evankmyran@gmail.com>
2025-06-25 13:20:52 -0500

    Update 02_08-auto1.c

--- CH02/02_08/02_08-auto1.c
+++ CH02/02_08/02_08-auto1.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+//fucntion 
 void sum(void)
 {
+	//auto is a keyword for scope
+	//when you declare an auto variable, it declares sotrage duration
+	//the variable gets stored on the stack when 
+	//however when the program leaves the scope that the variable is defined, this case the sum function
+	//the variables are destroyed from the stack
 	auto int a,b;
 
+	//define variables
 	a = 91; b = 7;
 	printf("%d + %d = %d\n",a,b,a+b);
 }
