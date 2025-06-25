9e72a92416c88bf26a3527286cab83a58e4beb53
Evan Myran <evankmyran@gmail.com>
2025-06-25 15:50:40 -0500

    pointer.c

--- CH02/02_08/02_08-static1.c
+++ CH02/02_08/02_08-static1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+//function declaration myname takes no arguments, returns a pointer a character, more commonly, a string in C (since strings are character arrays)
 char *myname(void)
 {
+	//initialize a char array that reads in the string of characters
 	char me[] = "Dan Gookin";
 
 	return(me);
